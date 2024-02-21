2417a8dd594f9ba58507c4899d9a9255e649e485
shl <shinhyung012@naver.com>
2024-02-21 14:56:32 +0900

    refactor FriendGroup

--- code/app/back_end/projectname/src/main/java/com/capstone/projectname/friendGroup/FriendGroup.java
+++ code/app/back_end/projectname/src/main/java/com/capstone/projectname/friendGroup/FriendGroup.java
@@ -13,4 +13,38 @@ public class FriendGroup {
     
     @Column(nullable = false)
     private String groupName;
+
+    public FriendGroup() {
+    	
+    }
+    
+	public FriendGroup(int groupId, String groupName) {
+		super();
+		this.groupId = groupId;
+		this.groupName = groupName;
+	}
+
+	public int getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	@Override
+	public String toString() {
+		return "FriendGroup [groupId=" + groupId + ", groupName=" + groupName + "]";
+	}
+	// 인텔리제이에 맞춰서 수정 필요
+    
+    
 }
