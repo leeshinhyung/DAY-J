36c62502880873c689b87afa827ed32a9a5c3b27
DNA-B <sphz3813@gmail.com>
2024-02-26 16:29:58 +0900

    add Update API

--- code/app/back_end/projectname/src/main/java/com/capstone/projectname/appUser/AppUserController.java
+++ code/app/back_end/projectname/src/main/java/com/capstone/projectname/appUser/AppUserController.java
@@ -1,6 +1,9 @@
 package com.capstone.projectname.appUser;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +24,11 @@ public class AppUserController {
     @GetMapping("/find-appUser-id")
     public AppUser findById() {
         return appUserRepository.findById(1).get();
+    }
+    
+    @PostMapping("/update-appUser")
+    public void update(@Valid @RequestBody AppUser appUser) {
+        AppUser savedAppUser = appUserRepository.save(appUser);
     }
     
     @GetMapping("/delete-appUser-id")
