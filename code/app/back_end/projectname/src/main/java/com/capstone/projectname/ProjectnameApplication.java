715d66d1d4328877678cb82c65515b029277acf9
DNA-B <sphz3813@gmail.com>
2024-02-27 23:53:48 +0900

    refact pom.xml

--- code/app/back_end/projectname/src/main/java/com/capstone/projectname/ProjectnameApplication.java
+++ code/app/back_end/projectname/src/main/java/com/capstone/projectname/ProjectnameApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectnameApplication {
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectnameApplication.class, args);
 	}
