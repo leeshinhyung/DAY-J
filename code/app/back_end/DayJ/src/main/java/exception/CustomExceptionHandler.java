ba923747685745438bf8a6fed3c52ee2c750aad0
DNA-B <sphz3813@gmail.com>
2024-04-03 19:12:21 +0900

    add exception in FriendGroup

---
+++ code/app/back_end/DayJ/src/main/java/exception/CustomExceptionHandler.java
@@ -0,0 +1,15 @@
+package exception;
+
+import com.capstone.dayj.friendGroup.FriendGroupNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@RestControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(FriendGroupNotFoundException.class)
+    protected ResponseEntity<?> handleFriendGroupNotFoundException(FriendGroupNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
